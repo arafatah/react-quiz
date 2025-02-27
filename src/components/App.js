5967d0bc5e452790c88f89e01c772ab0a5a6cf89
arafatah <arafathowlader890@gmail.com>
2025-02-27 11:38:18 +0600

    Enhance state management by adding answer handling in App component; update Options and Question components to reflect new answer logic

--- src/components/App.js
+++ src/components/App.js
@@ -32,13 +32,18 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -63,7 +68,13 @@ export default function App() {
           {status === "ready" && (
             <StartScreen numbQuestions={numbQuestions} dispatch={dispatch} />
           )}
-          {status === "active" && <Question question={questions[index]} />}
+          {status === "active" && (
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+          )}
         </Main>
       </div>
     </>
