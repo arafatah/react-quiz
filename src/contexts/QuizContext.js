b5b3ef10df0fcce229aadc3360c2cebea06659d2
arafatah <arafathowlader890@gmail.com>
2025-03-12 10:53:39 +0600

    Wrap App component with QuizProvider for improved context management

--- src/contexts/QuizContext.js
+++ src/contexts/QuizContext.js
@@ -115,7 +115,7 @@ function QuizProvider({ children }) {
 
 function useQuiz() {
   const context = useContext(QuizContext);
-  if (!context === undefined)
+  if (context === undefined)
     throw new Error("QuizContext was used outside of the QuizProvider");
   return context;
 }
